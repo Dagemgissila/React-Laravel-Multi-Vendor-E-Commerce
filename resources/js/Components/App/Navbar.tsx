bc782c0cc633e9e3d69c5d2c53f8ca3b6fbd5518
Dagemgisila <dagemgisila@gmail.com>
2025-04-16 07:55:03 +0300

    create and user navbar

--- resources/js/Components/App/Navbar.tsx
+++ resources/js/Components/App/Navbar.tsx
@@ -1,6 +1,8 @@
-import { Link } from "@inertiajs/react";
+import { Link, usePage } from "@inertiajs/react";
 
 export const Navbar = () => {
+    const { auth } = usePage().props;
+    const { user } = auth;
     return (
         <div className="navbar bg-base-100 shadow-sm">
             <div className="flex-1">
@@ -8,7 +10,7 @@ export const Navbar = () => {
                     laraStore
                 </Link>
             </div>
-            <div className="flex-none">
+            <div className="flex-none gap-4">
                 <div className="dropdown dropdown-end">
                     <div
                         tabIndex={0}
@@ -52,43 +54,58 @@ export const Navbar = () => {
                         </div>
                     </div>
                 </div>
-                <div className="dropdown dropdown-end">
-                    <div
-                        tabIndex={0}
-                        role="button"
-                        className="btn btn-ghost btn-circle avatar"
-                    >
-                        <div className="w-10 rounded-full">
-                            <img
-                                alt="Tailwind CSS Navbar component"
-                                src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
-                            />
+                {user && (
+                    <div className="dropdown dropdown-end">
+                        <div
+                            tabIndex={0}
+                            role="button"
+                            className="btn btn-ghost btn-circle avatar"
+                        >
+                            <div className="w-10 rounded-full">
+                                <img
+                                    alt="Tailwind CSS Navbar component"
+                                    src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+                                />
+                            </div>
                         </div>
-                    </div>
-                    <ul
-                        tabIndex={0}
-                        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
-                    >
-                        <li>
-                            <Link
-                                href={route("profile.edit")}
-                                className="justify-between"
-                            >
-                                Profile
-                            </Link>
-                        </li>
+                        <ul
+                            tabIndex={0}
+                            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+                        >
+                            <li>
+                                <Link
+                                    href={route("profile.edit")}
+                                    className="justify-between"
+                                >
+                                    Profile
+                                </Link>
+                            </li>
 
-                        <li>
-                            <Link
-                                href={route("logout")}
-                                method="post"
-                                as="button"
-                            >
-                                Logout
-                            </Link>
-                        </li>
-                    </ul>
-                </div>
+                            <li>
+                                <Link
+                                    href={route("logout")}
+                                    method="post"
+                                    as="button"
+                                >
+                                    Logout
+                                </Link>
+                            </li>
+                        </ul>
+                    </div>
+                )}
+                {!user && (
+                    <>
+                        <Link href={route("login")} className="btn">
+                            Login
+                        </Link>
+                        <Link
+                            href={route("register")}
+                            className="btn btn-primary"
+                        >
+                            Register
+                        </Link>
+                    </>
+                )}
             </div>
         </div>
     );
--- resources/js/Pages/Auth/Login.tsx
+++ resources/js/Pages/Auth/Login.tsx
@@ -3,7 +3,7 @@ import InputError from "@/Components/Core/InputError";
 import InputLabel from "@/Components/Core/InputLabel";
 import PrimaryButton from "@/Components/Core/PrimaryButton";
 import TextInput from "@/Components/Core/TextInput";
-import GuestLayout from "@/Layouts/GuestLayout";
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 import { Head, Link, useForm } from "@inertiajs/react";
 import { FormEventHandler } from "react";
 
@@ -29,7 +29,7 @@ export default function Login({
     };
 
     return (
-        <GuestLayout>
+        <AuthenticatedLayout>
             <Head title="Log in" />
 
             {status && (
@@ -37,74 +37,96 @@ export default function Login({
                     {status}
                 </div>
             )}
+            <div className="p-8">
+                <div className="card bg-white shadow mx-auto max-w-[420px]">
+                    <div className="card-body">
+                        <form onSubmit={submit}>
+                            <div>
+                                <InputLabel htmlFor="email" value="Email" />
 
-            <form onSubmit={submit}>
-                <div>
-                    <InputLabel htmlFor="email" value="Email" />
+                                <TextInput
+                                    id="email"
+                                    type="email"
+                                    name="email"
+                                    value={data.email}
+                                    className="mt-1 block w-full"
+                                    autoComplete="username"
+                                    isFocused={true}
+                                    onChange={(e) =>
+                                        setData("email", e.target.value)
+                                    }
+                                />
 
-                    <TextInput
-                        id="email"
-                        type="email"
-                        name="email"
-                        value={data.email}
-                        className="mt-1 block w-full"
-                        autoComplete="username"
-                        isFocused={true}
-                        onChange={(e) => setData("email", e.target.value)}
-                    />
+                                <InputError
+                                    message={errors.email}
+                                    className="mt-2"
+                                />
+                            </div>
 
-                    <InputError message={errors.email} className="mt-2" />
+                            <div className="mt-4">
+                                <InputLabel
+                                    htmlFor="password"
+                                    value="Password"
+                                />
+
+                                <TextInput
+                                    id="password"
+                                    type="password"
+                                    name="password"
+                                    value={data.password}
+                                    className="mt-1 block w-full"
+                                    autoComplete="current-password"
+                                    onChange={(e) =>
+                                        setData("password", e.target.value)
+                                    }
+                                />
+
+                                <InputError
+                                    message={errors.password}
+                                    className="mt-2"
+                                />
+                            </div>
+
+                            <div className="mt-4 block">
+                                <label className="flex items-center">
+                                    <Checkbox
+                                        name="remember"
+                                        checked={data.remember}
+                                        onChange={(e) =>
+                                            setData(
+                                                "remember",
+                                                (e.target.checked ||
+                                                    false) as false
+                                            )
+                                        }
+                                    />
+                                    <span className="ms-2 text-sm text-gray-600">
+                                        Remember me
+                                    </span>
+                                </label>
+                            </div>
+
+                            <div className="mt-4 flex items-center justify-end">
+                                {canResetPassword && (
+                                    <Link
+                                        href={route("password.request")}
+                                        className="rounded-md text-sm text-gray-600 underline hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                                    >
+                                        Forgot your password?
+                                    </Link>
+                                )}
+
+                                <PrimaryButton
+                                    className="ms-4"
+                                    disabled={processing}
+                                >
+                                    Log in
+                                </PrimaryButton>
+                            </div>
+                        </form>
+                    </div>
                 </div>
-
-                <div className="mt-4">
-                    <InputLabel htmlFor="password" value="Password" />
-
-                    <TextInput
-                        id="password"
-                        type="password"
-                        name="password"
-                        value={data.password}
-                        className="mt-1 block w-full"
-                        autoComplete="current-password"
-                        onChange={(e) => setData("password", e.target.value)}
-                    />
-
-                    <InputError message={errors.password} className="mt-2" />
-                </div>
-
-                <div className="mt-4 block">
-                    <label className="flex items-center">
-                        <Checkbox
-                            name="remember"
-                            checked={data.remember}
-                            onChange={(e) =>
-                                setData(
-                                    "remember",
-                                    (e.target.checked || false) as false
-                                )
-                            }
-                        />
-                        <span className="ms-2 text-sm text-gray-600">
-                            Remember me
-                        </span>
-                    </label>
-                </div>
-
-                <div className="mt-4 flex items-center justify-end">
-                    {canResetPassword && (
-                        <Link
-                            href={route("password.request")}
-                            className="rounded-md text-sm text-gray-600 underline hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                        >
-                            Forgot your password?
-                        </Link>
-                    )}
-
-                    <PrimaryButton className="ms-4" disabled={processing}>
-                        Log in
-                    </PrimaryButton>
-                </div>
-            </form>
-        </GuestLayout>
+            </div>
+        </AuthenticatedLayout>
     );
 }
